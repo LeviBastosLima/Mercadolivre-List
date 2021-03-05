e5e76b5249233ea068ebe655b81b7d1483ac7921
LeviBastosLima <lebaliro5@gmail.com>
2021-03-05 13:51:54 -0300

    Config to deploy on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vough_backend.wsgi
--- realtrends_test/settings.py
+++ realtrends_test/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,3 +124,5 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+
+django_heroku.settings(locals())
